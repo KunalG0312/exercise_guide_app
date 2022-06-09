6e749a59bac3658d15d87d336d69587b452a283b
KunalG0312 <harshgondhlekar@gmail.com>
2022-06-09 13:21:29 +0530

    nav bar added

---
+++ lib/dietPlans.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DietPlans extends StatelessWidget {
+  const DietPlans({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/healthTips.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HealthTips extends StatelessWidget {
+  const HealthTips({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
